a422dc0049af3ec5a672365eab4d1831cec7e9ea
EagleCollin <scoutcollin97@gmail.com>
2022-04-15 13:01:16 -0500

    Added saving throws and proficiency bonus

--- src/app/character-list/character.service.ts
+++ src/app/character-list/character.service.ts
@@ -15,7 +15,7 @@ export class CharacterService{
     {name: "James", class: "Wizard", level: 4,background: "Charlatan", race: "Elf",alignment:"Chaotic Good", attributes: {strength: 15, dexterity: 18, intelligence: 14, charisma: 15, wisdom: 17, constitution:12 }},
     {name: "Ron", class: "Barbarian", level: 2, background: "Criminal", race: "Ogre", alignment: "Neutral Evil", attributes: {strength: 14, dexterity: 12, intelligence: 17, charisma: 13, wisdom: 18, constitution: 15}},
     {name: "Nathan", class: "Artificer", level: 10, background: "Scholar", race: "Human", alignment: "Chaotic Neutral", attributes: {strength: 16, dexterity: 14, intelligence: 16, charisma: 10, wisdom: 14, constitution: 14}},
-    {name: "Charlotte", class: "Warlock", level: 8, background: "Mercenary", race: "Half-Elf", alignment: "Lawful Good", attributes: {strength: 12, dexterity: 15, intelligence: 10, charisma: 17, wisdom: 14, constitution: 15}}
+    {name: "Charlotte", class: "Warlock", level: 14, background: "Mercenary", race: "Half-Elf", alignment: "Lawful Good", attributes: {strength: 12, dexterity: 10, intelligence: 10, charisma: 17, wisdom: 14, constitution: 15}}
   ]
 
   showCharacters(){
@@ -39,9 +39,14 @@ export class CharacterService{
     return this.myCharacters.slice()[idx]
   }
 
-  showCharacterMods(attribute){
+  showCharacterMods(attribute:number){
     let abilityModifier = Math.floor((attribute-10)/2)
     return abilityModifier
+  }
+
+  showProfBonus(level:number){
+      let profBonus = 1 + (Math.ceil(level/4))
+      return profBonus
   }
 
 }
--- src/app/character/character-details/character-details.component.ts
+++ src/app/character/character-details/character-details.component.ts
@@ -14,32 +14,46 @@ export class CharacterDetailsComponent implements OnInit {
   character: Character;
 
   // Attribute Modifier Scores
-  strengthMod
-  dexMod
-  intMod
-  wisMod
-  conMod
-  charismaMod
+  strengthMod:number
+  dexMod:number
+  intMod:number
+  wisMod:number
+  conMod:number
+  charismaMod:number
 
   //skill modifier scores
-  acrobatics
-  animal
-  arcana
-  athletics
-  deception
-  history
-  insight
-  intimidation
-  investigation
-  medicine
-  nature
-  perception
-  performance
-  persuasion
-  religion
-  sleight
-  stealth
-  survival
+  acrobatics:number
+  animal:number
+  arcana:number
+  athletics:number
+  deception:number
+  history:number
+  insight:number
+  intimidation:number
+  investigation:number
+  medicine:number
+  nature:number
+  perception:number
+  performance:number
+  persuasion:number
+  religion:number
+  sleight:number
+  stealth:number
+  survival:number
+
+  //saving throw scores
+  strengthSave:number
+  wisdomSave:number
+  dexSave: number
+  intSave:number
+  charismaSave:number
+  conSave:number
+
+  //proficiency bonus
+  profBonus:number
+
+  //armor class
+  armorClass:number
 
   constructor(private route: ActivatedRoute, private characterService: CharacterService, private router: Router) {}
 
@@ -73,6 +87,19 @@ export class CharacterDetailsComponent implements OnInit {
       this.sleight = this.characterService.showCharacterMods(this.character.attributes.dexterity)
       this.stealth = this.characterService.showCharacterMods(this.character.attributes.dexterity)
       this.survival = this.characterService.showCharacterMods(this.character.attributes.wisdom)
+      //saving throws
+      this.conSave = this.characterService.showCharacterMods(this.character.attributes.constitution)
+      this.strengthSave = this.characterService.showCharacterMods(this.character.attributes.strength)
+      this.wisdomSave = this.characterService.showCharacterMods(this.character.attributes.wisdom)
+      this.intSave = this.characterService.showCharacterMods(this.character.attributes.intelligence)
+      this.charismaSave = this.characterService.showCharacterMods(this.character.attributes.charisma)
+      this.dexSave = this.characterService.showCharacterMods(this.character.attributes.dexterity)
+
+      //proficiency bonus
+      this.profBonus = this.characterService.showProfBonus(this.character.level)
+
+      //armor class
+      this.armorClass = 10 + this.dexMod
     });
 
   }
