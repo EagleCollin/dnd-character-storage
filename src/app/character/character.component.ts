0bd2f9cd95a04989336759fef3c7ccec06a713bf
EagleCollin <scoutcollin97@gmail.com>
2022-04-27 19:12:15 -0500

    Changing functionality

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -8,24 +8,30 @@ import {
 import { Character } from '../shared/characters/character-model';
 import { CharacterService } from '../character-list/character.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { relative } from 'path';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.css'],
 })
-export class CharacterComponent implements AfterViewInit {
+export class CharacterComponent implements OnInit {
   selectedChar: Character;
+  showCharacterList = true;
 
   constructor(
     private characterService: CharacterService,
-    private cdr: ChangeDetectorRef
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this.characterService.selectedCharacter.subscribe((currChar) => {
       this.selectedChar = currChar;
-      this.cdr.detectChanges();
     });
+    if (this.router.url.includes('/characters/')) {
+      this.showCharacterList = false;
+      return this.showCharacterList;
+    }
   }
 }
